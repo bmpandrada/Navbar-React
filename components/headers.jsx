f44e9d09750d01d91b8c8262c1eba9ae8d40a95f
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2022-07-02 23:14:12 +0800

    Initial commit

---
+++ components/headers.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbars">
+      <nav>
+        <div className="logo">React</div>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Navbar from "/components/headers.jsx";
+
+ReactDom.render(<Navbar />, document.getElementById("root"));
